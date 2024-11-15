967ede37123c3fdf5193f86daa3ac67afcfe9047
Tomo-Aot <vespaanalis833@gmail.com>
2024-11-15 19:49:08 +0900

    Update sem_practice.R

--- sem/sem_practice.R
+++ sem/sem_practice.R
@@ -11,6 +11,7 @@ library(tidySEM)
 HS.model <- ' visual  =~ x1 + x2 + x3
               textual =~ x4 + x5 + x6
               speed   =~ x7 + x8 + x9 '
+
 fit <- cfa(HS.model, data=HolzingerSwineford1939)
 
 graph_sem(model = fit)
