fa695a7f4c23ef2e678684754d09a0a5eb08cae6
Tomo-Aot <vespaanalis833@gmail.com>
2024-11-17 09:45:38 +0900

    python file has uploaded
    Nov 17, 2024, i added a python file for structural equation modeling file

---
+++ sem/sem_python.py
@@ -0,0 +1,11 @@
+# structural equation modeling on python
+# 2024-11-15
+### Import Necessary Libraries
+import pandas as pd
+import semopy
+import matplotlib
+import numpy as np
+
+
+# structural equation modeling on python
+
