02c5fef604cde3b448d4bfac4008725ecf54a625
Tomo-Aot <vespaanalis833@gmail.com>
2024-11-27 00:43:18 +0900

    Add files via upload

--- Data_analysis/pca_workspace.R
+++ Data_analysis/pca_workspace.R
@@ -64,6 +64,9 @@ mat = scale(df[,c(1:4)], center = TRUE, scale = TRUE)
 # 主成分分析の結果
 result = prcomp(mat)
 
+# 要約統計量の確認
+summary(result)
+
 result |> 
   ggbiplot::ggbiplot(groups = df$Species) + 
   theme(
@@ -76,6 +79,7 @@ result |>
 
 # 図を保存します。
 # 80㎜＊80mmがベストですが、種名やベクトルがはみ出すので少し大きくして保存
+# 図を保存する際は、一度pdfにしてからpngに変換すると調節しやすいです
 pdfname = "./image/iris_pca_ggbiplot.pdf"
 pngname = str_replace(pdfname, pattern = "pdf", replacement = "png")
 
@@ -90,19 +94,38 @@ image_read_pdf(pdfname, density = 300) |>
 # ggbiplot()の場合はこの辺りがすこし不便です。
 
 
-# ggplot()で作図します
+# 次はggplot()で作図します
+# 主成分をtibbleにして元のデータの種名を加えます
 pc = result$x |> 
   as_tibble() |> 
   mutate(Species = df$Species)
 
 # 類似性のプロット
 # まだベクトルはつけていません
-pc |> 
+plot = pc |> 
   ggplot() + 
   geom_point(
     aes(x = PC1, y = PC2, colour = Species)
   ) + 
+  scale_x_continuous(
+    name = "PC1",
+    limits = c(-3.5, 4),
+    breaks = seq(-3.5, 4, length = 6)
+  ) + 
+  scale_y_continuous(
+    name = "PC2",
+    limits = c(-3, 3),
+    breaks = seq(-3, 3, length = 6)
+  ) + 
+  theme(
+    legend.position = c(1, 1),
+    legend.justification = c(1, 1),
+    legend.background = element_blank(),
+    legend.direction = "vertical"
+  ) + 
   scale_colour_viridis_d(end = 0.8)
+
+print(plot)
 
 # 図を保存します
 pdfname = "./image/iris_pca.pdf"
@@ -112,5 +135,96 @@ ggsave(filename = pdfname, height = height,
        width = width, units = "mm")
 
 image_read_pdf(pdfname, density = 300) |> 
+  image_write(pngname)
+
+# 矢印を追加します
+# 矢印の大きさは主成分分析のRotationの係数で決まります
+rot = result$rotation
+
+plot2 = plot + 
+  geom_segment(
+    aes(x = 0, y = 0,
+        xend = PC1, yend = PC2),
+    data = rot,
+    arrow = arrow(type = "closed", length = unit(1.2, "mm"))
+  )
+
+plot2
+
+pdfname = "./image/iris_pca_arw.pdf"
+pngname = str_replace(pdfname, pattern = "pdf", replacement = "png")
+
+ggsave(filename = pdfname, height = height, 
+       width = width, units = "mm")
+
+image_read_pdf(pdfname, density = 600) |> 
+  image_write(pngname)
+
+# 各主成分の寄与率を計算して、図の軸に表示します
+# 寄与率の確認方法がわからないので、計算します
+ctb = function(x){
+  sdev = x
+  
+  # 各主成分の分散
+  variance = sdev ^ 2
+  # 総分散
+  total_variance = sum(variance)
+  
+  # 各主成分の寄与率（%）
+  contribution = variance / total_variance * 100
+  
+  return(contribution)
+}
+
+# 寄与率と主成分軸をまとめます
+per = 
+  ctb(result$sdev) |> 
+  as_tibble() |> 
+  mutate(PC = colnames(result$x))
+
+# 最終的な図は次のようになる
+# 今回は寄与率の整数部分だけ表示します
+xlab = str_c("PC1 (", floor(per[1, 1]), "%)")
+ylab = str_c("PC2 (", floor(per[2, 1]), "%)")
+
+pc |> 
+  ggplot() + 
+  geom_point(
+    aes(x = PC1, y = PC2, colour = Species)
+  ) + 
+  geom_segment(
+    aes(x = 0, y = 0,
+        xend = PC1, yend = PC2),
+    data = rot,
+    arrow = arrow(type = "open", length = unit(1, "mm"), angle = 20),
+    size = 0.2
+  ) + 
+  scale_x_continuous(
+    name = xlab,
+    limits = c(-3.5, 4.5),
+    breaks = seq(-3.5, 4.5, length = 5)
+  ) + 
+  scale_y_continuous(
+    name = ylab,
+    limits = c(-3, 4),
+    breaks = seq(-3, 4, length = 6)
+  ) + 
+  theme(
+    legend.position = c(1, 1),
+    legend.justification = c(1, 1),
+    legend.background = element_blank(),
+    legend.direction = "vertical",
+    panel.border = element_rect(size = 1, fill = NA)
+  ) + 
+  scale_colour_viridis_d(end = 0.8)
+
+# 図を保存します
+pdfname = "./image/iris_pca_final.pdf"
+pngname = str_replace(pdfname, pattern = "pdf", replacement = "png")
+
+ggsave(filename = pdfname, height = height, 
+       width = width, units = "mm")
+
+image_read_pdf(pdfname, density = 600) |> 
   image_write(pngname)
 
