4803d893cf0a7cd94d49b7da4cab25cd6fd05fd7
Tomo-Aot <vespaanalis833@gmail.com>
2025-01-12 00:55:23 +0900

    Add files via upload

--- new_function_workspace/new_function_lab.R
+++ new_function_workspace/new_function_lab.R
@@ -19,11 +19,11 @@ x = c(- pi, pi) |> as_tibble()
 y = c(- pi, pi) |> as_tibble()
 
 x = x |> expand(
-  value = seq(min(value), max(value), length = 500)
+  value = seq(min(value), max(value), length = 5)
 )
 
 y = y |> expand(
-  value = seq(min(value), max(value), length = 500)
+  value = seq(min(value), max(value), length = 5)
 )
 
 circle = x |> 
@@ -62,7 +62,6 @@ label = tibble(
   y = 1,
   label = "Circle"
 )
-
 
 circle |> 
   ggplot() + 
@@ -118,7 +117,7 @@ triangle = function(n){
   }
 }
 
-triangle |> 
+triangle(n = 7) |> 
   ggplot() + 
   geom_polygon(
     aes(x = x, y = y)
@@ -135,9 +134,87 @@ triangle |>
 # image_read_pdf(pdfname, density = 300) %>%
 #   image_write(pngname)
 
-# make gif
+# 任意の三角形を作る関数を作成します。
+tr_ang = function(a, b, deg_c, fill){
+  # 二篇の長さをa, bとします。
+  # 二篇の鋏角をangleとします。
+  x1 = 0
+  y1 = 0
+  x2 = a
+  y2 = 0
+  
+  # 度数法で入力するのが分かりやすいと思うので、
+  # 入力値を度で表して、出力の際に弧度法に直します。
+  angle = deg_c * pi / 180
+  
+  x3 = b / a * cos(angle)
+  y3 = b / a * sin(angle)
+  df = tibble(
+    x = c(x1, x2, x3),
+    y = c(y1, y2, y3)
+  )
+  
+  # 軸の範囲を揃えます。
+  # 最小値
+  min_df = 
+    function(df){
+      if(min(df$x) > min(df$y)){
+        min = min(df$y)
+      } else{
+        min = min(df$x)
+      }
+    }
+  
+  # 最大値
+  max_df = 
+    function(df){
+      if(max(df$x) < max(df$y)){
+        max = max(df$y)
+      } else{
+        max = max(df$x)
+      }
+      return(max)
+    }
+  
+  # プロット
+  p = df |> 
+    ggplot() + 
+    geom_polygon(
+      aes(x = x, y = y),
+      fill = fill
+    ) + 
+    scale_x_continuous(
+      limits = c(min_df(df), max_df(df)),
+      breaks = seq(min_df(df), max_df(df), length = 6)
+    ) + 
+    scale_y_continuous(
+      limits = c(min_df(df), max_df(df)),
+      breaks = seq(min_df(df), max_df(df), length = 6)
+    ) + 
+    theme(
+      axis.title = element_blank(),
+      axis.ticks = element_line(colour = "black")
+    )
+  
+  # 出力
+  return(p)
+}
 
 
+tr_ang(
+  a = 3,
+  b = 4,
+  deg_c = 60,
+  fill = "turquoise"
+)
 
 
-
+#       (x3, y3)
+#       /\
+#      /  \
+#     /    \
+#  b /      \ 
+#   /        \
+#  / angle    \
+# /____________\(x2, y2)
+#        a
